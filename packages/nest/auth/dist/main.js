47bc5f062e03b2eaf48d2a13842373fa8732351a
joaogotardelo <joao.gotardelo@picpay.com>
2022-09-09 11:30:56 -0300

    New Open Telemetry tracing with basic logs

--- packages/nest/auth/dist/main.js
+++ packages/nest/auth/dist/main.js
@@ -2,10 +2,22 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const auth_module_1 = require("./auth.module");
+const sdk_node_1 = require("@opentelemetry/sdk-node");
+const auto_instrumentations_node_1 = require("@opentelemetry/auto-instrumentations-node");
+const api_1 = require("@opentelemetry/api");
 async function bootstrap() {
+    initOtel();
     const app = await core_1.NestFactory.create(auth_module_1.AuthModule);
     app.enableCors();
     await app.listen(3000);
+}
+function initOtel() {
+    api_1.diag.setLogger(new api_1.DiagConsoleLogger(), api_1.DiagLogLevel.INFO);
+    const sdk = new sdk_node_1.NodeSDK({
+        traceExporter: new sdk_node_1.tracing.ConsoleSpanExporter(),
+        instrumentations: [(0, auto_instrumentations_node_1.getNodeAutoInstrumentations)()]
+    });
+    sdk.start();
 }
 bootstrap();
 //# sourceMappingURL=main.js.map
--- packages/nest/auth/src/main.ts
+++ packages/nest/auth/src/main.ts
@@ -1,9 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AuthModule } from './auth.module';
+import { NodeSDK, tracing } from '@opentelemetry/sdk-node';
+import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
+import { diag, DiagConsoleLogger, DiagLogLevel } from '@opentelemetry/api';
 
 async function bootstrap() {
+  initOtel();
+
   const app = await NestFactory.create(AuthModule);
   app.enableCors();
   await app.listen(3000);
 }
+
+function initOtel() {
+  diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
+
+  const sdk = new NodeSDK({
+    traceExporter: new tracing.ConsoleSpanExporter(),
+    instrumentations: [getNodeAutoInstrumentations()]
+  });
+
+  sdk.start();
+}
+
 bootstrap();
