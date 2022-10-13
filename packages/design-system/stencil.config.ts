0036f90f0a365bfb6cb1af6f7c3e1281626acf64
joaogotardelo <joao.gotardelo@picpay.com>
2022-10-12 21:42:32 -0300

    First version of design tokens

--- packages/design-system/stencil.config.ts
+++ packages/design-system/stencil.config.ts
@@ -2,20 +2,10 @@ import { Config } from '@stencil/core';
 
 export const config: Config = {
   namespace: 'design-system',
+  globalStyle: 'src/styles/scss-var.scss',
   outputTargets: [
     {
-      type: 'dist',
-      esmLoaderPath: '../loader',
-    },
-    {
-      type: 'dist-custom-elements',
-    },
-    {
-      type: 'docs-readme',
-    },
-    {
-      type: 'www',
-      serviceWorker: null, // disable service workers
+      type: 'www'
     },
   ],
 };
